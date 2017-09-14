3e0ce74103fa7e3d144d8ce3c09fcaa5bfa67606
zsynuting <zsynuting@163.com>
2017-09-14 18:12:37 +0800

    remote sub components

--- src/components/calendar/calendar.vue
+++ src/components/calendar/calendar.vue
@@ -1,18 +1,28 @@
 <template>
-	<div>
+	<div class="calendar-container">
 		<div>
-		<span class='float-cell'>
-			<CalendarHeader v-for="item in weekdays" :weekday="item" />
-			<CalendarCell v-for="dateItem in allDates" :date="dateItem" :date-cell="dateItem" />
-			<div class='clear-float'></div>
-		</span>
+			<div class='float-cell'>
+				<div class="selector">
+					<select v-model="selectedMonth" class="width: 30%">
+						<option v-for="month in months" :value="month.index">{{month.name}}</option>
+					</select>
+				</div>
+				<div class="selector">
+				</div>
+				<div class='clear-float'></div>
+			</div>
+			<span class='float-cell'>
+				<span class="calendar-header" v-for="weekday in weekdays">{{weekday}}</span>
+				<div v-bind:class="['cell', {current: dateItem.isCurrent}, {currentmonth: dateItem.isCurrentMonth}]" v-for="dateItem in allDates">
+					{{dateItem.date.getDate()}}
+				</div>
+				<div class='clear-float'></div>
+			</span>
 		</div>
 	</div>
 </template>
 
 <script>
-	import CalendarHeader from './calendarheader'
-	import CalendarCell from './calendarcell'
 
 
 	export default{
@@ -24,9 +34,61 @@
 					minDate: new Date(0, 1, 1),
 					maxDate: new Date(9999, 12, 31),
 					currentDate: null,
+					selectedMonth: null,
+					selectedYear: null,
 					startDate: null,
 					endDate: null,
-					allDates: []
+					allDates: [],
+					months: [
+					{
+						name: "Jan",
+						index: 0
+					},					
+					{
+						name: "Feb",
+						index: 1
+					},
+					{
+						name: "Mar",
+						index: 2
+					},
+					{
+						name: "Apr",
+						index: 3
+					},
+					{
+						name: "May",
+						index: 4
+					},
+					{
+						name: "Jun",
+						index: 5
+					},
+					{
+						name: "Jul",
+						index: 6
+					},
+					{
+						name: "Aug",
+						index: 7
+					},
+					{
+						name: "Sep",
+						index: 8
+					},
+					{
+						name: "Oct",
+						index: 9
+					},
+					{
+						name: "Nov",
+						index: 10
+					},
+					{
+						name: "Dec",
+						index: 11
+					}
+					]
 				};
 		 	obj.currentDate = this.$options.propsData.selectedDate ? 
 		 		new Date(this.$options.propsData.selectedDate) : new Date(now.getFullYear(), now.getMonth(), now.getDate());
@@ -44,6 +106,9 @@
 
 		 	obj.startDate = new Date(firstDayOfCurrentMonth.getTime() - daysOfPrevMonth * 24 * 60 * 60 * 1000);
 		 	obj.endDate = new Date(lastDayOfCurrentMonth.getTime() + daysOfNextMonth * 24 * 60 * 60 * 1000);
+		 	obj.selectedYear = obj.currentDate.getFullYear();
+		 	obj.selectedMonth = obj.currentDate.getMonth();
+		 	
 		 	
 		 	var tempDate = new Date(obj.startDate);
 		 	while(tempDate <= obj.endDate) {
@@ -58,19 +123,59 @@
 
 		},
 		components: {
-			CalendarHeader: CalendarHeader,
-			CalendarCell: CalendarCell
 		}
 	}
 
 </script>
 
 <style scoped>
+	.calendar-container{
+		width: 336px;
+	}
 	.float-cell{
 		float: left;
 		width: 336px;
 	}
 	.clear-float{
 		clear: both;
+	}
+	.selector{
+		float: left;
+		display: inline-block;
+		box-sizing: border-box;
+        -moz-box-sizing: border-box; 
+        -webkit-box-sizing: border-box;
+		width: 50%
+	}
+	.calendar-header{
+		display: inline-block;
+		width: 48px;
+		height: 48px;
+		text-align: center;
+		line-height: 48px;
+		font-size: 1em;
+	}
+	.cell{
+		display: inline-block;
+		width: 48px;
+		height: 48px;
+		text-align: center;
+		line-height: 48px;
+		font-size: 1em;
+		color: #aaa;
+		cursor: pointer;
+		box-sizing: border-box;
+        -moz-box-sizing: border-box; 
+        -webkit-box-sizing: border-box;
+        border: 1px solid #fff;
+	}
+	.cell:hover{
+		border: 1px solid;
+	}
+	.current{
+		background-color: lightgrey;
+	}
+	.currentmonth{
+		color: #000;
 	}
 </style>
---
+++ src/components/calendar/monthselector.vue
@@ -0,0 +1,15 @@
+<template>
+	<select v-model="selectedMonth">
+		<option v-for="month in months" v-bind:value="month.index">{{month.name}}</option>
+	</select>
+</template>
+
+<script>
+	export default {
+		props: ["months", "selectedMonth"]
+	}
+</script>
+
+<style>
+
+</style>
